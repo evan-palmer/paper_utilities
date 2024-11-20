d1641bab2d469b8c5cc7abac8c9bf5b6c537fe4f
Evan Palmer <evanp922@gmail.com>
2024-11-20 00:30:15 -0800

    Added missing functions to module

--- paper_utilities/analytics/__init__.py
+++ paper_utilities/analytics/__init__.py
@@ -1,3 +1,7 @@
-from .statistics import calculate_stats
+from .statistics import (
+    calculate_stats,
+    calculate_stats_across_dfs,
+    merge_and_calculate_stats,
+)
 
-__all__ = ["calculate_stats"]
+__all__ = ["calculate_stats", "merge_and_calculate_stats", "calculate_stats_across_dfs"]
